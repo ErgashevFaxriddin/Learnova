42813f751c6391f67e6104a1e7acd456e503de82
faxriddin ergashev <uzcodermelody@gmail.com>
2026-04-04 23:28:07 +0500

    admin panel updated

--- users/models.py
+++ users/models.py
@@ -1,2 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('student', 'Student'),
+        ('teacher', 'Teacher'),
+        ('admin', 'Admin'),
+    )
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+    groups = models.ManyToManyField(
+        Group,
+        related_name='custom_user_set',
+        blank=True
+    )
+
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='custom_user_permissions_set',
+        blank=True
+    )
+
+    def __str__(self):
+        return self.username
+
+
+class StudentProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='student_profile')
+    level = models.CharField(max_length=50)
+    experience_points = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user.username
+
+
+class TeacherProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='teacher_profile')
+    specialization = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.user.username
